9848af17a6ec49a688541e9496701b555696d605
Ale Pereira <naitwa-alexandre@gmail.com>
2025-06-13 16:02:09 -0300

    adds powerups & countdown

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private GameObject focalPoint;
+    [SerializeField] private float powerupStrength = 15.0f;
+    [SerializeField] private float timePowerupLasts = 7.0f;
+    [SerializeField] private GameObject powerupIndicator;
 
     private Rigidbody rb;
+
+    public bool hasPower;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +23,38 @@ public class PlayerController : MonoBehaviour
     {
         float fowardInput = Input.GetAxis("Vertical");
         rb.AddForce(fowardInput * speed * focalPoint.transform.forward);
+
+        powerupIndicator.transform.position = new(transform.position.x, -0.5f, transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            hasPower = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPower)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log($"Collided with {collision.gameObject.name} with powerup set to {hasPower}");
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(timePowerupLasts);
+        Debug.Log("Powerup faded");
+        hasPower = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
