6f03576cfdfe37277427a337780ad40ca7800343
Ale Pereira <naitwa-alexandre@gmail.com>
2025-06-13 22:19:13 -0300

    finished the project

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,17 +9,27 @@ public class Enemy : MonoBehaviour
     [Header("Configurações de movimento")]
     [SerializeField, Tooltip("Velocidade do inimigo")]
     private float m_Speed = 5.0f;
+    private bool isGameOver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Rb = GetComponent<Rigidbody>();
         m_Player = GameObject.Find("Player");
+        isGameOver = m_Player.GetComponentInParent<PlayerController>().gameOver;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Rb.AddForce(GetLookDirection() * m_Speed);
+        if (!isGameOver)
+        {
+            m_Rb.AddForce(GetLookDirection() * m_Speed);
+        }
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private Vector3 GetLookDirection()
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,21 +3,46 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject m_EnemyPrefab;
+    [SerializeField] private GameObject m_PowerupPrefab;
 
-    private readonly float spawnRange = 9;
+    private readonly float spawnRange = 9f;
+
+    private float numberOfEnemies = 1;
+
+    public int enemyCount;
 
     // Update is called once per frame
     void Start()
     {
-        SpawnEnemy();
+        SpawnEnemyWave(numberOfEnemies);
+        Instantiate(m_PowerupPrefab, GenerateRandomPosition(), m_PowerupPrefab.transform.rotation);
     }
 
-    private void SpawnEnemy()
+    private void Update()
     {
-        Instantiate(m_EnemyPrefab, GetRandomPosition(), m_EnemyPrefab.transform.rotation);
+        SpawnNewEnemyWave();
     }
 
-    private Vector3 GetRandomPosition()
+    private void SpawnEnemyWave(float enemiesToRespawn)
+    {
+        for(int i = 0; i < enemiesToRespawn; i++)
+        {
+            Instantiate(m_EnemyPrefab, GenerateRandomPosition(), m_EnemyPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnNewEnemyWave()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(++numberOfEnemies);
+            Instantiate(m_PowerupPrefab, GenerateRandomPosition(), m_PowerupPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateRandomPosition()
     {
         return new Vector3(GetRandomCoordinates(), 0, GetRandomCoordinates());
     }
